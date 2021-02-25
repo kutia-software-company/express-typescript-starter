6cf448855b7ec7bddde66dabcc9d52ca05ad6e87
Gentrit Abazi <gentritabazi01@gmail.com>
2021-02-25 21:38:27 +0100

    Add more values to .env

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,7 +5,11 @@ export const appConfig = {
     isProduction: env('NODE_ENV') === 'production',
     isStaging: env('NODE_ENV') === 'staging',
     isDevelopment: env('NODE_ENV') === 'development',
-   
     name: env('APP_NAME'),
     port: env('APP_PORT'),
+    routePrefix: env('APP_ROUTE_PREFIX'),
+
+    controllers: env('CONTROLLERS'),
+    middlewares: env('MIDDLEWARES'),
+    interceptors: env('INTERCEPTORS')
 }
