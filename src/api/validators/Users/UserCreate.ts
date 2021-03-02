a9e61c01479aa3b29cee3b3eca025f72e472667f
Gentrit Abazi <gentritabazi01@gmail.com>
2021-03-02 20:33:29 +0100

    Add more rules on UserCreate.ts.

--- src/api/validators/Users/UserCreate.ts
+++ src/api/validators/Users/UserCreate.ts
@@ -1,7 +1,14 @@
-import { IsNotEmpty, IsEmail } from 'class-validator'
+import { IsNotEmpty, IsEmail, IsString, MinLength, MaxLength } from 'class-validator'
 
 export class UserCreate {
     @IsNotEmpty()
+    @IsString()
     @IsEmail()
     public email: string
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    @MaxLength(20)
+    public password: string
 }
