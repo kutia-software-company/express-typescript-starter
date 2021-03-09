509eb29a74989bcd1d4d5093fe74bb5387f674f6
Gentrit Abazi <gentritabazi01@gmail.com>
2021-03-09 21:20:09 +0100

    Socket io 👌.

---
+++ src/api/controllers/Chat/ChatSocketController.ts
@@ -0,0 +1,26 @@
+import { OnConnect, SocketController, ConnectedSocket, OnDisconnect, MessageBody, OnMessage } from 'socket-controllers'
+import { Service } from 'typedi'
+
+@Service()
+@SocketController()
+export class ChatSocketController {
+    @OnConnect()
+    connection(@ConnectedSocket() socket: any) {
+        console.log('Client connected.')
+        return socket
+    }
+
+    @OnDisconnect()
+    disconnect(@ConnectedSocket() socket: any) {
+        console.log('Client disconnected.')
+    }
+
+    @OnMessage('save-message')
+    save(@ConnectedSocket() socket: any, @MessageBody() message: any) {
+        console.log('Received message: ', message)
+        console.log('Setting id to the message and sending it back to the client.')
+        message.id = 1
+
+        socket.emit('message-saved', message)
+    }
+}
