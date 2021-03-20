1a2627a1a7cf3f25e0bd626f2f77cc9b6271ce74
Gentrit Abazi <gentritabazi01@gmail.com>
2021-03-20 19:33:42 +0100

    Improve index.ts 💕

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,92 @@
 import 'reflect-metadata'
 import { appConfig } from './config/app'
-import { Application } from 'express'
-import { createExpressServer, useContainer as routingControllersUseContainer } from 'routing-controllers'
+import { useContainer as routingControllersUseContainer, useExpressServer } from 'routing-controllers'
 import { Container } from 'typedi'
 import { createConnection, useContainer as typeormOrmUseContainer } from 'typeorm'
 import { Container as containerTypeorm } from 'typeorm-typedi-extensions'
 import { eventDispatcher } from './utils/eventDispatcher'
-import { createSocketServer, useContainer as socketUseContainer } from 'socket-controllers'
+import { useSocketServer, useContainer as socketUseContainer } from 'socket-controllers'
 import { registerController as registerCronJobs, useContainer as cronUseContainer } from 'cron-decorators'
+import * as path from 'path'
+import express from 'express'
 
-routingControllersUseContainer(Container)
-typeormOrmUseContainer(containerTypeorm)
-socketUseContainer(Container)
-cronUseContainer(Container)
+export class App {
+    private app: express.Application = express()
+    private port: Number = appConfig.port
 
-// Define port
-const port = appConfig.port || 3000
+    public constructor() {
+        this.bootstrap()
+    }
 
-// Create typeorm connection
-createConnection().then(async connection => {
-    // Load subscribers
-    eventDispatcher()
+    public async bootstrap() {
+        this.useContainers()
+        await this.typeOrmCreateConnection()
+        this.registerEvents()
+        this.registerCronJobs()
+        this.serveStaticFiles()
+        this.registerSocketControllers()
+        this.registerRoutingControllers()
+        this.registerDefaultHomePage()
+    }
 
-    // Register cron jobs
-    if ((appConfig.cronJobsEnabled)) {
+    private useContainers() {
+        routingControllersUseContainer(Container)
+        typeormOrmUseContainer(containerTypeorm)
+        socketUseContainer(Container)
+        cronUseContainer(Container)
+    }
+
+    private async typeOrmCreateConnection() {
+        return await createConnection()
+    }
+
+    private registerEvents() {
+        return eventDispatcher()
+    }
+
+    private registerCronJobs() {
+        if (!appConfig.cronJobsEnabled) {
+            return false
+        }
+
         registerCronJobs([__dirname + appConfig.cronJobsDir])
     }
 
-    // Create a new express server instance
-    const expressApp: Application = createExpressServer({
-        validation: { stopAtFirstError: true },
-        cors: true,
-        classTransformer: true,
-        defaultErrorHandler: false,
-        routePrefix: appConfig.routePrefix,
-        controllers: [__dirname + appConfig.controllersDir],
-        middlewares: [__dirname + appConfig.middlewaresDir]
-    })
+    private serveStaticFiles() {
+        this.app.use('/public', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }))
+    }
 
-    // Define a route handler for the default home page
-    expressApp.get('/', (req, res) => {
-        res.json({ title: appConfig.name, mode: appConfig.node, date: new Date() })
-    })
+    private registerSocketControllers() {
+        const server = require('http').Server(this.app)
+        const io = require('socket.io')(server)
 
-    // Start the Express server
-    expressApp.listen(port, () => {
-        console.log(`🚀 Server started at http://localhost:${port}`)
-    })
+        this.app.use(function(req: any, res: any, next) {
+            req.io = io
+            next()
+        })
+        
+        server.listen(this.port, () => console.log(`🚀 Server started at http://localhost:${this.port}`))
 
-    // Socket.io
-    createSocketServer(3001, { controllers: [__dirname + appConfig.controllersDir] })
-}).catch(error => console.log('Error: ', error))
+        useSocketServer(io, { controllers: [__dirname + appConfig.controllersDir] })
+    }
+
+    private registerRoutingControllers() {
+        useExpressServer(this.app, {
+            validation: { stopAtFirstError: true },
+            cors: true,
+            classTransformer: true,
+            defaultErrorHandler: false,
+            routePrefix: appConfig.routePrefix,
+            controllers: [__dirname + appConfig.controllersDir],
+            middlewares: [__dirname + appConfig.middlewaresDir]
+        })
+    }
+
+    private registerDefaultHomePage() {
+        this.app.get('/', (req, res) => {
+            res.json({ title: appConfig.name, mode: appConfig.node, date: new Date() })
+        })
+    }
+}
+
+new App
