d5f6b245d9648c6bdd3e826c2dd3d4c57e462836
Gentrit Abazi <gentritabazi01@gmail.com>
2021-03-21 19:07:43 +0100

    Remove Service Decorator from UserRepository.

--- src/api/repositories/Users/UserRepository.ts
+++ src/api/repositories/Users/UserRepository.ts
@@ -1,9 +1,7 @@
-import { Service } from 'typedi'
 import { User } from '../../models/Users/User'
 import { EntityRepository } from 'typeorm'
 import { RepositoryBase } from '../../../abstracts/RepositoryBase'
 
-@Service()
 @EntityRepository(User)
 export class UserRepository extends RepositoryBase<User>  {
     // 
