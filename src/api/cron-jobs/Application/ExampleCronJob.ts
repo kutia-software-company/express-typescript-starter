c4bd34349b712d6697142338dfc544d970b116de
Gentrit Abazi <gentritabazi01@gmail.com>
2021-03-31 21:57:43 +0200

    Rename ExampleJob to ExampleCronJob.

--- src/api/cron-jobs/Application/ExampleCronJob.ts
+++ src/api/cron-jobs/Application/ExampleCronJob.ts
@@ -2,8 +2,8 @@ import { CronController as CronJobClass, Cron } from 'cron-decorators'
 import { Service as Injectable } from 'typedi'
 
 @Injectable()
-@CronJobClass('example-job')
-export class ExampleJob {
+@CronJobClass('example-cron-job')
+export class ExampleCronJob {
     // Async cron job that runs every second.
     @Cron('Log every second', '* * * * * *')
     public async secCronJob(): Promise<void> {
