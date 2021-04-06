b1c94141bb4ec373ea86d740283e333b0aba1ec1
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-06 22:52:30 +0200

    Add config.

---
+++ src/config/mail.ts
@@ -0,0 +1,11 @@
+import { env } from '../utils/env';
+
+export const mailConfig = {
+    provider: env('MAIL_PROVIDER'),
+    host: env('MAIL_HOST'),
+    port: Number(env('MAIL_PORT')),
+    authUser: env('MAIL_AUTH_USER'),
+    authPassword: env('MAIL_AUTH_PASSWORD'),
+    fromName: env('MAIL_FROM_NAME'),
+    fromAddress: env('MAIL_FROM_ADDRESS')
+}
