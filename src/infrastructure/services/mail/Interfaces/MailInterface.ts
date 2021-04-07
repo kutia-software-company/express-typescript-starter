35ca5e7a559ae4309bcf2d57080a75097f7c3e6b
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-07 08:14:35 +0200

    Add Classes.

---
+++ src/infrastructure/services/mail/Interfaces/MailInterface.ts
@@ -0,0 +1,13 @@
+export interface MailInterface {
+    from(value: string): this;
+
+    to(value: string): this;
+
+    subject(value: string): this;
+
+    text(value: string): this;
+
+    html(value: string): this;
+
+    send(): Promise<any>;
+}
---
+++ src/infrastructure/services/mail/Providers/SmtpProvider.ts
@@ -0,0 +1,64 @@
+import * as nodeMailer from 'nodemailer'
+import { mailConfig } from '../../../../config/mail'
+
+export class SmtpProvider {
+    private transporter: nodeMailer.Transporter
+    private fromValue: string = mailConfig.fromName + ' ' + mailConfig.authUser
+    private toValue: string
+    private subjectValue: string
+    private textValue: string
+    private htmlValue: string
+
+    public constructor() {
+        this.transporter = nodeMailer.createTransport({
+            host: mailConfig.host,
+            port: mailConfig.port,
+            auth: {
+                user: mailConfig.authUser,
+                pass: mailConfig.authPassword
+            }
+        })
+    }
+
+    public from(value: string) {
+        this.fromValue = value
+
+        return this
+    }
+
+    public to(value: string) {
+        this.toValue = value
+
+        return this
+    }
+
+    public subject(value: string) {
+        this.subjectValue = value
+
+        return this
+    }
+
+    public text(value: string) {
+        this.textValue = value
+
+        return this
+    }
+
+    public html(value: string) {
+        this.htmlValue = value
+
+        return this
+    }
+
+    public async send() {
+        const mailOptions = {
+            from: this.fromValue,
+            to: this.toValue,
+            subject: this.subjectValue,
+            text: this.textValue,
+            html: this.htmlValue
+        }
+
+        return await this.transporter.sendMail(mailOptions)
+    }
+}
