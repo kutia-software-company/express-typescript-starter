f753c2473a2ef5aac97e719c9299b6f5a7ea7336
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-08 21:46:17 +0200

    Create Queue.

---
+++ src/abstracts/QueueJobBase.ts
@@ -0,0 +1,30 @@
+import { Job, Queue, Worker, Processor } from 'bullmq'
+
+export abstract class QueueJobBase {
+    private queue: Queue
+    abstract readonly jobName: string = QueueJobBase.name
+    private data: any
+
+    abstract handle(job: any): Promise<Processor<any, any, string>> | any
+
+    public constructor(data: any) {
+        this.data = data
+    }
+
+    public process() {
+        this.queue = new Queue(this.jobName)
+        this.queue.add(this.jobName, this.data)
+        const worker = new Worker(this.jobName, async (job: Job) => { this.handle(job) })
+
+        worker.on('completed', this.onCompleted)
+        worker.on('failed', this.onFailed)
+    }
+
+    public onCompleted(job: any): any {
+        // 
+    }
+
+    public onFailed(job: any): any {
+        // 
+    }
+}
