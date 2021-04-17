57c6b41bf284a355063089d566a63cd806bbb953
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-17 21:04:58 +0200

    Add suport for Mailgen.

---
+++ src/infrastructure/services/mail/MailGenerator.ts
@@ -0,0 +1,27 @@
+import Mailgen from 'mailgen'
+import { Service } from 'typedi'
+import { appConfig } from '@base/config/app'
+
+@Service()
+export class MailGenerator {
+    private readonly mailGenerator: Mailgen
+
+    constructor() {
+        this.mailGenerator = new Mailgen({
+            theme: 'default',
+            product: {
+                // Appears in header & footer of e-mails
+                name: appConfig.name,
+                link: appConfig.url
+            }
+        })
+    }
+
+    generatePlaintext(params: Mailgen.Content): string {
+        return this.mailGenerator.generatePlaintext(params)
+    }
+
+    generateHtmlContent(params: Mailgen.Content): string {
+        return this.mailGenerator.generate(params)
+    }
+}
---
+++ src/infrastructure/services/mail/templates/ForgotPasswordTemplate.ts
@@ -0,0 +1,24 @@
+import Mailgen from 'mailgen'
+import { Service } from 'typedi'
+import { appConfig } from '@base/config/app'
+
+@Service()
+export class ForgotPasswordTemplate {
+    static getTemplate(name: string, email: string, forgotKey: string): Mailgen.Content {
+        return {
+            body: {
+                name,
+                intro: 'You have received this email because a password reset request for your account was received.',
+                action: {
+                    instructions: 'Click the button below to reset your password:',
+                    button: {
+                        color: '#DC4D2F',
+                        text: 'Reset your password',
+                        link: `${appConfig.url}/reset-password?email=${email}&key=${forgotKey}`
+                    }
+                },
+                outro: 'If you did not request a password reset, no further action is required on your part.'
+            }
+        }
+    }
+}
