4d49ba557112639f65238265e8bd5ff455405246
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-18 22:11:08 +0200

    Improve Migrations.

--- src/database/migrations/1618771301779-CreateUsersTable.ts
+++ src/database/migrations/1618771301779-CreateUsersTable.ts
@@ -1,24 +1,24 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
-export class CreateUsersTable1614879263311 implements MigrationInterface {
+export class CreateUsersTable1618771301779 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         const table = new Table({
             name: 'users',
             columns: [
-                { name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: 'increment' },
+                { name: 'id', type: 'bigint', isPrimary: true, isGenerated: true, generationStrategy: 'increment' },
                 { name: 'first_name', type: 'varchar', length: '191' },
                 { name: 'last_name', type: 'varchar', length: '191' },
                 { name: 'email', type: 'varchar', length: '191' },
-                { name: 'password', type: 'varchar', length: '191' }
+                { name: 'password', type: 'varchar', length: '191' },
+                { name: 'role_id', type: 'bigint' }
             ]
-        })
+        });
 
         await queryRunner.createTable(table);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        return await queryRunner.dropTable('users');
     }
 
 }
