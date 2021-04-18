c35282c9bdf0d124c6e21efba072e3690749859a
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-18 04:22:03 +0200

    Improve env function.

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,12 @@
 import * as dotenv from 'dotenv'
 dotenv.config() // { path: `.env.${process.env.NODE_ENV}` }
 
-export function env(key: string): string {
+export function env(key: string, defaultValue: null | string = null): string {
+
+    return process.env[key] ?? defaultValue as string
+}
+
+export function envOrFail(key: string): string {
     if (typeof process.env[key] === 'undefined') {
         throw new Error(`Environment variable ${key} is not set.`);
     }
