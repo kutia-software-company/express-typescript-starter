c1c690b676d1bd9f1704ecf430baa3969406b375
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-18 22:13:04 +0200

    Add CreateRolesTable.

---
+++ src/database/migrations/1618771206804-CreateRolesTable.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateRolesTable1618771206804 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const table = new Table({
+            name: 'roles',
+            columns: [
+                { name: 'id', type: 'bigint', isPrimary: true, isGenerated: true, generationStrategy: 'increment' },
+                { name: 'name', type: 'varchar', length: '50', isUnique: true }
+            ]
+        });
+
+        await queryRunner.createTable(table);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        return await queryRunner.dropTable('roles');
+    }
+
+}
