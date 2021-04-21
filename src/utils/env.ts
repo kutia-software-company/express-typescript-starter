96c72ceb9d1ed4af4fec028ca44678c90cd922d4
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-21 11:26:12 +0200

    Remove space.

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,7 +2,6 @@ import * as dotenv from 'dotenv'
 dotenv.config() // { path: `.env.${process.env.NODE_ENV}` }
 
 export function env(key: string, defaultValue: null | string = null): string {
-
     return process.env[key] ?? defaultValue as string
 }
 
