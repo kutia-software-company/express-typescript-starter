6e60a266c03376afbd2be2ea0c6a8897a1f7bc5b
Gentrit Abazi <gentritabazi01@gmail.com>
2021-04-24 22:28:31 +0200

    Add docusaurus.

---
+++ docs/README.md
@@ -0,0 +1,25 @@
+# Intro
+
+This website is built using [Docusaurus 2](https://v2.docusaurus.io/), a modern static website generator.
+
+## Installation
+
+```console
+npm install
+```
+
+## Local Development
+
+```console
+npm start
+```
+
+This command starts a local development server and open up a browser window. Most changes are reflected live without having to restart the server.
+
+## Build
+
+```console
+npm build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
---
+++ docs/sidebars.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'all-docs': [
+    {
+      type: 'category',
+      label: 'Getting started',
+      items: [
+        'getting-started/introduction',
+        'getting-started/quick-start',
+        'getting-started/installation',
+        'getting-started/project-structure',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Documentation',
+      items: [
+        'documentation/controllers',
+        'documentation/http-errors',
+        'documentation/middlewares',
+        'documentation/interceptors',
+        'documentation/decorators',
+        'documentation/scheduling-jobs',
+        'documentation/openapi',
+        'documentation/query-params'
+      ],
+    }
+  ],
+};
