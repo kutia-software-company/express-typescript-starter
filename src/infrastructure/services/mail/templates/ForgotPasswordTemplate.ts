b01d9f6fb0769fc5853cc932698b6dfef338a347
Gentrit Abazi <gentritabazi01@gmail.com>
2021-06-05 14:17:47 +0200

    Create MailTemplateBase.

--- src/infrastructure/services/mail/templates/ForgotPasswordTemplate.ts
+++ src/infrastructure/services/mail/templates/ForgotPasswordTemplate.ts
@@ -1,20 +1,31 @@
 import Mailgen from 'mailgen';
 import { Service } from 'typedi';
 import { appConfig } from '@base/config/app';
+import { MailGenerator } from '../MailGenerator';
+import { MailTemplateBase } from '@base/infrastructure/abstracts/MailTemplateBase';
 
 @Service()
-export class ForgotPasswordTemplate {
-  static getTemplate(name: string, email: string, forgotKey: string): Mailgen.Content {
+export class ForgotPasswordTemplate extends MailTemplateBase {
+  private username: string;
+  private token: string;
+
+  constructor(username: string, email: string, token: string) {
+    super();
+    this.username = username;
+    this.token = token;
+  }
+
+  getTemplate(): Mailgen.Content {
     return {
       body: {
-        name,
+        name: this.username,
         intro: 'You have received this email because a password reset request for your account was received.',
         action: {
           instructions: 'Click the button below to reset your password:',
           button: {
             color: '#DC4D2F',
             text: 'Reset your password',
-            link: `${appConfig.url}/reset-password?email=${email}&key=${forgotKey}`,
+            link: `${appConfig.url}/reset-password?token=${this.token}`,
           },
         },
         outro: 'If you did not request a password reset, no further action is required on your part.',
